50f748c292887cf35d6e985a6d1621febb344c91
PemudaTampan555 <flash24576@gmail.com>
2025-12-11 07:20:57 +0700

    Menambahkan function tambahData

--- MagangApp.java
+++ MagangApp.java
@@ -25,6 +25,55 @@ public class MagangApp {
         } while();
     }
 
+    public static void tambahData() {
+        System.out.println("\n=== TAMBAH DATA PENDAFTAR ===");
+
+        System.out.print("Nama mahasiswa: ");
+        String nama = input.nextLine();
+
+        System.out.print("NIM: ");
+        String nim = input.nextLine();
+
+        System.out.print("Program Studi: ");
+        String prodi = input.nextLine();
+
+        System.out.print("Perusahaan tujuan: ");
+        String perusahaan = input.nextLine();
+
+        String semester;
+        while (true) {
+            System.out.print("Semester (6 atau 7): ");
+            semester = input.nextLine();
+
+            if (semester.equals("6") || semester.equals("7")) break;
+
+            System.out.println("ERROR! Semester hanya boleh 6 atau 7.");
+        }
+
+        String status;
+        while (true) {
+            System.out.print("Status (Diterima/Menunggu/Ditolak): ");
+            status = input.nextLine();
+
+            if (status.equalsIgnoreCase("Diterima") ||
+                status.equalsIgnoreCase("Menunggu") ||
+                status.equalsIgnoreCase("Ditolak")) {
+                break;
+            }
+            System.out.println("ERROR! Input status tidak valid.");
+        }
+
+        data[jumlahData][0] = nama;
+        data[jumlahData][1] = nim;
+        data[jumlahData][2] = prodi;
+        data[jumlahData][3] = perusahaan;
+        data[jumlahData][4] = semester;
+        data[jumlahData][5] = status;
+        jumlahData++;
+
+        System.out.println("Data pendaftaran magang berhasil ditambahkan. Total pendaftar: " + jumlahData);
+    }
+
     
     
 }
